34e7bb3ac93b41337eb35b25192fcd9c69e83f06
Ambientes Virtuales IUPB <ambientesvirtuales@pascualbravo.edu.co>
2026-03-22 10:28:19 -0500

    sonidoss

---
+++ Assets/_Game/Scripts/Menu/Velocimetro.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Velocimetro : MonoBehaviour
+{
+    public Vector2 angulos;
+    float t;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t = MotoController.singleton.maci
+        transform.localEulerAngles = Vector3.forward;
+    }
+}
--- Assets/_Game/Scripts/Zombies/Zombie.cs
+++ Assets/_Game/Scripts/Zombies/Zombie.cs
@@ -5,9 +5,15 @@ public class Zombie : MonoBehaviour
     public RagdollController ragdollController;
     public GameObject sangre;
     public float desaceleracion=1;
+    public GameObject[] objetosDesactivables;
+    public AudioSource audio;
     private void Start()
     {
         Destroy(gameObject,40);
+        for (int i = 0; i < objetosDesactivables.Length; i++)
+        {
+            objetosDesactivables[i].SetActive(Random.Range(0f, 1f) < 0.7f);
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -22,6 +28,7 @@ public class Zombie : MonoBehaviour
     {
         ragdollController.ActivarMuerte();
         sangre.SetActive(true);
+        audio.Play();
         MotoController.singleton.ReduceSpeed(desaceleracion);
     }
 }
